2cdf77c585eb8a479a1aa643e6532fb7e2a978dd
jebson <159553167+IzabeleHarlei@users.noreply.github.com>
2024-05-08 11:56:46 -0300

    react
    continuar os codigos do slide

--- izaapp/scr/screen/index.tsx
+++ izaapp/scr/screen/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text} from "react-native"
 export function SignIn() {
   return(
-    <View>
+    <View style={{flex:1, backgroundColor: 'red', alignItems: "center", justifyContent:'center'}}>
       <Text>Bom dia</Text>
     </View>
   );
