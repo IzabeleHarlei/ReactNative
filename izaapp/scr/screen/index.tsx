b3e886c557157032ddc3b338e8fe4b504d8b2096
JemmEdu16 <159553167+IzabeleHarlei@users.noreply.github.com>
2024-05-24 11:59:16 -0300

    continuaçaõ slide
    slide 55

--- izaapp/scr/screen/index.tsx
+++ izaapp/scr/screen/index.tsx
@@ -1,13 +1,14 @@
 
-import {View, Text,TextInput} from "react-native";
+import {View, Text,TextInput,Image} from "react-native";
 import React, {useState} from "react";
 import { styles } from './styles';
-import IllustratuionImg from  '' 
+import IllustratuionImg from '../assets/illustration.png';
+
 export function SignIn(){
 return(
   <View style={styles.container}>
-
-    <Image source={} /> 
+    <StatusBar barSlyte='light-content' backgroudColor={"transparente"} transLucent/>
+     <Image source={IllustratuionImg} style={styles.image} resizeMode='stretch'/> 
 
   </View>
 );
