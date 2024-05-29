ff4e7b24e73d137002421df49dd3516bc2dc9a8e
JemmEdu16 <159553167+IzabeleHarlei@users.noreply.github.com>
2024-05-29 11:54:49 -0300

    sle 56
    slide 56

--- izaapp/scr/screen/index.tsx
+++ izaapp/scr/screen/index.tsx
@@ -3,13 +3,26 @@ import {View, Text,TextInput,Image} from "react-native";
 import React, {useState} from "react";
 import { styles } from './styles';
 import IllustratuionImg from '../assets/illustration.png';
+import { StatusBar } from "expo-status-bar";
 
 export function SignIn(){
 return(
   <View style={styles.container}>
-    <StatusBar barSlyte='light-content' backgroudColor={"transparente"} transLucent/>
-     <Image source={IllustratuionImg} style={styles.image} resizeMode='stretch'/> 
+    <StatusBar barStyle= 'light-content' backgroundColor={"transparent"} transLucent />
+     <Image source={IllustratuionImg} /> 
+  <View style={styles.content}> 
+      
+    <Text style = {styles.title}>
+      Organize Suas {'\n'}
+      Jogatinas {'\n'}
+      facilmente
+    </Text>
+    <Text style={styles.subtitle}>
+      Crie grupos para jogar seus games{'\n'}
+      favoritos com seus amigos
 
+    </Text>
+  </View>
   </View>
 );
 
